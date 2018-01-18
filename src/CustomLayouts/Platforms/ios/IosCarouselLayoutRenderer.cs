1a669909a5067c02e73dcee63445d5f6103d1fc0
andy840119 <andy840119@gmail.com>
2018-01-18 18:32:14 +0800

    把renderer集合到同一個專案

---
+++ src/CustomLayouts/Platforms/ios/IosCarouselLayoutRenderer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Xamarin.Forms;
+using CustomLayouts.Controls;
+using CustomLayouts.Platforms.ios;
+using Xamarin.Forms.Platform.iOS;
+using UIKit;
+using System.ComponentModel;
+using System.Drawing;
+using CustomLayouts.Controls.CarouselLayout;
+using CoreGraphics;
+
+[assembly:ExportRenderer(typeof(CarouselLayout), typeof(CarouselLayoutRenderer))]
+
+namespace CustomLayouts.Platforms.ios
+{
+    public class CarouselLayoutRenderer : ScrollViewRenderer
+    {
+        UIScrollView _native;
+
+        public CarouselLayoutRenderer()
+        {
+            PagingEnabled = true;
+            ShowsHorizontalScrollIndicator = false;
+        }
+
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null) return;
+
+            _native = (UIScrollView)NativeView;
+            _native.Scrolled += NativeScrolled;
+            e.NewElement.PropertyChanged += ElementPropertyChanged;
+        }
+
+        bool hasNavigation = true;
+
+        void NativeScrolled(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!hasNavigation)
+                {
+                    //if goes in here, it means this page has no navigation bar
+                    //make horizontal scroll only
+                    //2018/1/18 : xamarin's bug
+                    _native.ContentSize = new CGSize(_native.ContentSize.Width, _native.Frame.Size.Height - 25);
+                }
+
+                var center = _native.ContentOffset.X + (_native.Bounds.Width / 2);
+                ((CarouselLayout)Element).SelectedIndex = ((int)center) / ((int)_native.Bounds.Width);
+            }
+            catch
+            {
+                hasNavigation = false;
+            }
+        }
+
+        void ElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == CarouselLayout.SelectedIndexProperty.PropertyName && !Dragging)
+            {
+                ScrollToSelection(false);
+            }
+        }
+
+        void ScrollToSelection(bool animate)
+        {
+            if (Element == null) return;
+
+            _native.SetContentOffset(new CoreGraphics.CGPoint
+                (_native.Bounds.Width *
+                    Math.Max(0, ((CarouselLayout)Element).SelectedIndex),
+                    _native.ContentOffset.Y),
+                animate);
+        }
+
+        public override void Draw(CoreGraphics.CGRect rect)
+        {
+            base.Draw(rect);
+            ScrollToSelection(false);
+        }
+    }
+}
+
+
